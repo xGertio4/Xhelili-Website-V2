6c7ea803ade2a1b069c3b4c75bbaade6e4e21ffc
GertioXhelili <gertixhelil@gmail.com>
2023-05-27 21:55:49 +0200

    finished email send

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -531,4 +531,26 @@ function initOwl(){
     });
   });
 
+  function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_22de8ft";
+    const templateID = "template_5bcutpg";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
   
